0564043617e421aa8617f3a30fe5593fdb00c805
sherifessam98 <sherifessam1998@gmail.com>
2023-07-15 21:44:11 -0400

    inital commit

---
+++ LighTool/main.py
@@ -0,0 +1,36 @@
+import hou
+from PySide2 import QtCore, QtUiTools, QtWidgets
+
+class Lomus(QtWidgets.QWidget):
+	def __init__(self):
+		super(Lomus, self).__init__()
+		ui_file = '/Users/sherifessam/Desktop/HoudiniLightingTool/Lomus.ui'
+		self.ui = QtUiTools.QUiLoader().load(ui_file)
+		self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
+		self.ui.lightCreate.clicked.connect(self.buttonClicked)
+		self.initialize_scene()
+
+
+	def initialize_scene(self):
+		global sceneRoot
+		sceneRoot = hou.node('/obj/')
+
+	def show(self):
+	    self.ui.show()
+
+	def buttonClicked(self):
+		self.lightCreator()
+
+
+	def lightCreator(self):
+		self.light_node = sceneRoot.createNode('light')
+		self.geo_node = sceneRoot.node('/obj/donut')
+		self.light_node.parm('lookat').set(self.geo_node.path())
+
+
+
+
+
+
+win = Lomus()
+win.show()
