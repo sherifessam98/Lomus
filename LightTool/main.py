bac51845a2849a6c71ab5167c5863d11215878e0
sherifessam98 <sherifessam1998@gmail.com>
2023-07-17 20:17:46 -0400

    Light creation added

--- LightTool/main.py
+++ LightTool/main.py
@@ -9,8 +9,8 @@ class Lomus(QtWidgets.QWidget):
         self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
         self.x = 0
         self.initialize_scene()
-        self.ui.lightCreate.clicked.connect(self.buttonClicked())
-        # self.clicked_before = False
+        self.ui.lightCreate.clicked.connect(self.buttonClick)
+
 
     def initialize_scene(self):
         global sceneRoot
@@ -19,20 +19,19 @@ class Lomus(QtWidgets.QWidget):
     def show(self):
         self.ui.show()
 
+    def buttonClick(self):
+        self.lightCreator()
+
+
     def lightCreator(self):
         self.geo_node = hou.node('/obj/donut')
-        self.light_node = sceneRoot.createNode('light')
-        self.light_node.setInput(0, self.geo_node)
+        if self.x < 4:
+            self.light_node = sceneRoot.createNode('light')
+            self.light_node.setInput(0, self.geo_node)
+            self.x += 1
+        else:
+            hou.ui.displayMessage("Already 4 lights in the scene", title="Error", severity=hou.severityType.Error)
 
-    def buttonClicked(self):
-        # if self.clicked:
-        self.x += 1
-        for self.x in range(1, 5):
-            if self.x < 5:
-                self.lightCreator()
-            else:
-                hou.ui.displayMessage("Already 4 lights in the scene", title="Error", severity=hou.severityType.Error)
-        # self.ui.lightCreate.clicked.connect(self.buttonClicked())
 
 
 win = Lomus()
